7da3c027731951fe7210b1a8c0d5f6c0a7d7d514
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-10 15:54:44 -0700

    Adds parse_template function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,14 @@
+import re
+
 def read_template(path):
     with open(path) as file:
         return file.read()
+
+
+def parse_template(text):
+    regex = r'\{(.*?)\}'
+    parts = tuple(re.findall(regex, text))
+    stripped = re.sub(regex, '{}', text)
+    return stripped, parts
 
 
