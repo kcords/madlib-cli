11311edadee9dfb50a9bebd1caa7a09c9b13b371
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-12 11:20:31 -0700

    Adds header and divider functions and tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,4 +1,16 @@
 import re
+import os
+
+
+def create_madlib():
+    show_header()
+
+
+def show_header():
+    show_divider()
+    print(strings["welcome"])
+    show_divider()
+
 
 def read_template(path):
     with open(path) as file:
@@ -16,3 +28,15 @@ def merge(madlib, user_inputs):
     return madlib.format(*user_inputs)
 
 
+def show_divider(character="="):
+    terminal_width = os.get_terminal_size().columns
+    divider_line = character * terminal_width
+    print(divider_line)
+
+
+strings = {
+    "welcome": """Hey there! Welcome to madlibs!\n\nYou will be prompted to select a madlib template.\n\nYou will then be given a series of prompts. Fill in each to create your madlib.\n\nReady to have some fun? Lets get started..."""
+}
+
+if __name__ == "__main__":
+    create_madlib()
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,5 +1,34 @@
 import pytest
-from madlib_cli.madlib import read_template, parse_template, merge
+import os
+from collections import namedtuple
+import sys
+
+from madlib_cli.madlib import show_header, show_divider, read_template, parse_template, merge
+
+
+def emulate_terminal_size(monkeypatch, columns=20, lines=10):
+    terminal_size = namedtuple('TerminalSize', ['columns', 'lines'])
+    monkeypatch.setattr(os, 'get_terminal_size', lambda: terminal_size(columns=columns, lines=lines))
+
+
+def text_in_captured_output(text, capsys):
+    captured = capsys.readouterr()
+    return text in captured.out
+
+
+def test_displays_a_valid_header(monkeypatch, capsys):
+    emulate_terminal_size(monkeypatch)
+    welcome_msg = "Welcome to madlibs!"
+    show_header()
+    assert text_in_captured_output(welcome_msg, capsys)
+
+
+
+def test_displays_a_valid_divider(monkeypatch, capsys):
+    emulate_terminal_size(monkeypatch)
+    divider = "==="
+    show_header()
+    assert text_in_captured_output(divider, capsys)
 
 
 def test_read_template_returns_stripped_string():
